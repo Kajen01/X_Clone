39415b2d1b089480348c4f64086ea9f0fd727cfa
Kajen01 <nkajenthiran22@gmail.com>
2024-12-10 02:35:45 +0530

    signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
+import './App.css';
+import {Routes, Route} from "react-router-dom"
+import SignUpPage from './pages/auth/signup/SignUpPage';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        {/* <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} /> */}
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
     </div>
   );
 }
