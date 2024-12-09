cc8cd1573fb0e901c63a01c0b143b56a363c4fbc
Kajen01 <nkajenthiran22@gmail.com>
2024-12-10 03:01:36 +0530

    home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {Routes, Route} from "react-router-dom"
 import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import HomePage from './pages/home/HomePage';
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
       <Routes>
-        {/* <Route path='/' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} /> */}
+        <Route path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
         <Route path='/signup' element={<SignUpPage />} />
       </Routes>
     </div>
